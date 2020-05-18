60e97947c4915e591db997c1849a740eecf0ab95
Mike8123 <50879711+Mike8123@users.noreply.github.com>
2020-05-18 15:38:27 +0300

    Added comments

--- Main.py
+++ Main.py
@@ -1,21 +1,25 @@
+# Importing libraries
 from bs4 import BeautifulSoup
 import requests
 
+# Requesting html-page from the url
 def get_html(url):
     result = requests.get(url)
     return result.text
 
+# Reciev data from html-page
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
     random_qustion1 = soup.find('div', {'class' : 'random_question'})
     print(random_qustion1.text)
 
+# Main function which inicialize other functions
 def main():
     html = get_html('https://db.chgk.info/random')
     get_data(html)
     gen_dic()
 
-
+# Generation dictionary with placeholders
 def gen_dic():
     dic1 = {
         'Турнир' : 'Название турнира',
@@ -28,5 +32,6 @@ def gen_dic():
     print(dic1.get('Вопрос'))
     print(dic1.get('Ответ'))
 
+# Fuck, I almost don't know about anything of this
 if __name__ == '__main__':
     main()
